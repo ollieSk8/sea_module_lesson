144da450a70995c541e831c99a4348b5dc3b296b
ollieSk8 <412537738@qq.com>
2015-05-21 12:22:37 +0800

    sea use

---
+++ js/module1.js
@@ -0,0 +1,9 @@
+define(function(require,exports,module){
+	//exports:对外提供接口的对象
+
+	function show(){
+		alert(1);
+	}
+
+	exports.show=show;
+});
---
+++ js/module2.js
@@ -0,0 +1,9 @@
+define(function(require,exports,module){
+	//exports:对外提供接口的对象
+
+	function hehe(){
+		alert(1);
+	}
+
+	exports.hehe=hehe;
+});
